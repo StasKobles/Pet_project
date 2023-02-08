f17b078cb5dfb6b85576f92705f01885eae2c669
Stas Kobles <sskobles@gmail.com>
2023-02-08 20:45:10 +0400

    add babel, extract plugin i18n. add jest and tests

--- config/build/buildLoaders.ts
+++ config/build/buildLoaders.ts
@@ -21,7 +21,19 @@ export function buildLoaders({ isDev }: BuildOptions): RuleSetRule[] {
             'sass-loader',
         ],
     };
-
+    const babelLoader = {
+        test: /\.(js|jsx|tsx)$/,
+        exclude: /node_modules/,
+        use: {
+            loader: 'babel-loader',
+            options: {
+                presets: ['@babel/preset-env'],
+                plugins: [['i18next-extract', {
+                    locales: ['ru', 'en'], keyAsDefaultValue: true,
+                }]],
+            },
+        },
+    };
     const typescriptLoader = {
         test: /\.tsx?$/,
         use: 'ts-loader',
@@ -39,5 +51,5 @@ export function buildLoaders({ isDev }: BuildOptions): RuleSetRule[] {
             },
         ],
     };
-    return [typescriptLoader, cssLoader, svgLoader, fileLoader];
+    return [fileLoader, svgLoader, babelLoader, typescriptLoader, cssLoader];
 }
