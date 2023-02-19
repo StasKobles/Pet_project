e70f7fc51a671ad02999621f2746da3c93f7acb6
Kobles Stas <sskobles@gmail.com>
2023-02-19 21:41:34 +0400

    fix dev plugins

--- config/build/buildPlugins.ts
+++ config/build/buildPlugins.ts
@@ -17,7 +17,7 @@ export function buildPlugins({
     paths,
     isDev,
 }: BuildOptions): WebpackPluginInstance[] {
-    return [
+    const plugins = [
         new HtmlWebpackPlugin({
             template: paths.html,
         }),
@@ -29,8 +29,11 @@ export function buildPlugins({
         new DefinePlugin({
             __IS_DEV__: JSON.stringify(isDev),
         }),
-        (isDev && new HotModuleReplacementPlugin(),
-        new ReactRefreshWebpackPlugin()),
-        new BundleAnalyzerPlugin({ openAnalyzer: false }),
     ];
+    if (isDev) {
+        plugins.push(new HotModuleReplacementPlugin());
+        plugins.push(new ReactRefreshWebpackPlugin());
+        plugins.push(new BundleAnalyzerPlugin({ openAnalyzer: false }));
+    }
+    return plugins;
 }
