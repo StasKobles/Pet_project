ae87addd2ef551fc55577836e1dd755338a2ff46
Kobles Stas <sskobles@gmail.com>
2023-03-22 05:58:01 +0400

    Testing feature authByUsername. TestAsyncThunk

---
+++ src/features/AuthByUsername/model/slice/loginSlice.test.ts
@@ -0,0 +1,21 @@
+import { DeepPartial } from '@reduxjs/toolkit';
+import { StateSchema } from 'app/providers/StoreProvider';
+import { LoginSchema } from '../types/loginSchema';
+import { loginActions, loginReducer } from './loginSlice';
+
+describe('loginSlice.test', () => {
+    test('test set username', () => {
+        const state: DeepPartial<LoginSchema> = { username: '123' };
+        expect(loginReducer(
+            state as StateSchema,
+            loginActions.setUsername('12321321'),
+        )).toEqual({ username: '12321321' });
+    });
+    test('test set password', () => {
+        const state: DeepPartial<LoginSchema> = { password: '123' };
+        expect(loginReducer(
+            state as StateSchema,
+            loginActions.setPassword('12321321'),
+        )).toEqual({ password: '12321321' });
+    });
+});
