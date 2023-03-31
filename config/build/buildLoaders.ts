b0218cbb9c038b3e33539dc7cf00bc9edfca81cc
Kobles Stas <sskobles@gmail.com>
2023-03-31 06:15:18 +0400

    add ReactRefreshWebpackPlugin, some useful stuff

--- config/build/buildLoaders.ts
+++ config/build/buildLoaders.ts
@@ -1,22 +1,12 @@
 import { RuleSetRule } from 'webpack';
+import { buildBabelLoader } from './loaders/buildBabelLoaders';
 import { buildCssLoader } from './loaders/buildCssLoaders';
 import { BuildOptions } from './types/config';
 
-export function buildLoaders({ isDev }: BuildOptions): RuleSetRule[] {
+export function buildLoaders(options: BuildOptions): RuleSetRule[] {
+    const { isDev } = options;
     const cssLoader = buildCssLoader(isDev);
-    const babelLoader = {
-        test: /\.(js|jsx|tsx)$/,
-        exclude: /node_modules/,
-        use: {
-            loader: 'babel-loader',
-            options: {
-                presets: ['@babel/preset-env'],
-                plugins: [['i18next-extract', {
-                    locales: ['ru', 'en'], keyAsDefaultValue: true,
-                }]],
-            },
-        },
-    };
+    const babelLoader = buildBabelLoader(options);
     const typescriptLoader = {
         test: /\.tsx?$/,
         use: 'ts-loader',
