418bbfa85f7813cb516db21af47c9e54e78d91ba
Stas Kobles <sskobles@gmail.com>
2023-04-21 16:52:01 +0400

    Articles EntityAdapter,thunks,slices,View selector

--- src/entities/Article/index.ts
+++ src/entities/Article/index.ts
@@ -1,5 +1,7 @@
 export { getArticleDetailsData } from './model/selectors/articleDetails';
-export type { Article, ArticleView } from './model/types/article';
+export type { Article } from './model/types/article';
 export type { ArticleDetailsSchema } from './model/types/articleDetailsSchema';
 export { ArticleDetails } from './ui/ArticleDetails/ArticleDetails';
 export { ArticleList } from './ui/ArticleList/ArticleList';
+export { ArticleView } from './model/types/article';
+export { ArticleViewSelector } from './ui/ArticleViewSelector/ArticleViewSelector';
